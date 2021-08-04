09da59740daced83523f2918e6bea7ff4ad83c23
Niklas Ertle <hockeytertle@gmail.com>
2021-08-03 18:57:05 -0600

    Change fetch request and values to use less code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,28 +12,7 @@ var getWorkout = function () {
     event.preventDefault();
     var workoutCat= $('#workout-cat').val();
     
-    fetch('https://wger.de/api/v2/exercisecategory/')
-    .then(function (response){
-        if (response.staus === 404) {
-            console.log('Error');
-        }else {
-            return response.json();
-        };
-    }).then(function (data) {
-        if (data.staus === 404) {
-            console.log('Error');
-        }else {
-            console.log(data.results);
-            // Get id# from results to be used comparing the name to the selected workout
-            var categoryId = 10
-            findExcercises(categoryId);
-        };
-    });
-};
-
-// Gets the workout list based on categorys from wger API
-function findExcercises (categoryId) {
-    var exerciseUrl = 'https://wger.de/api/v2/exercise/?limit=10&category=' + categoryId;
+    var exerciseUrl = 'https://wger.de/api/v2/exercise/?limit=10&category=' + workoutCat;
 
     fetch(exerciseUrl)
     .then(function (response){
