db6a3a8361e2081e3449de89fa77e4d09c73e2ef
Sheyla Popovich <Sheylapopovich@gmail.com>
2021-08-05 18:43:07 -0600

    added bulma styling to HTML/CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,3 +137,17 @@ function shuffleWorkouts (workouts) {
 // Event Listeners
 loadSiteBtn.click(showForm);
 genWorkoutForm.submit(getWorkout);
+
+
+
+// adding on modal
+$(".modal-button").click(function() {
+    var target = $(this).data("target");
+    $("html").addClass("is-clipped");
+    $(target).addClass("is-active");
+ });
+ 
+ $(".modal-close").click(function() {
+    $("html").removeClass("is-clipped");
+    $(this).parent().removeClass("is-active");
+ });
